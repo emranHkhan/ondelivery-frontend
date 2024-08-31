ca3df9cf31f5f129a5ab71541108cff549c6239d
emranHKhan <emran.khan2400@gmail.com>
2024-08-31 15:07:09 +0600

    contact css file name fixed

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import "./contact.css"
+import "./Contact.css"
 
 const Contact = () => {
     const map = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3689.4290034229093!2d91.83218937589872!3d22.375179140176154!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30ad27f8024d5501%3A0xb226fa7719072195!2sOxygen%2C%20Chattogram!5e0!3m2!1sen!2sbd!4v1720974433202!5m2!1sen!2sbd" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"'
